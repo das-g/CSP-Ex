83c04a4d4b5455ed3ac5c5e5ac7f552d2d27eef6
Raphael Borun Das Gupta <git@raphael.dasgupta.ch>
2009-03-29 15:13:43 +0200

    Ising with single spin flip MC

--- ising/ising.cpp
+++ ising/ising.cpp
@@ -1,8 +1,11 @@
 #include "../lattice_pbc/CLattice.h"
+#include <cstdlib> // for random()
+#include <iostream>
+#include <cmath> // for exp()
 
 typedef int spin_t;
 #define spin_up 1
-#define spin_down 0
+#define spin_down -1
 
 int main(){
 	
@@ -14,6 +17,39 @@ int main(){
 		for (int j=0; j<grid.Ny(); j++) {
 			for (int k=0; k<grid.Nz(); k++) {
 				grid(i,j,k)=spin_up;
+			}
+		}
+	}
+	
+	//srandom(1); //optionally seed the random generator
+	const int MC_STEPS = 1000000;
+	const double betaTwoJ = 1.;
+	
+	for (int step=0; step < MC_STEPS; ++step) {
+		
+		// choose a random site
+		int x = (random() * grid.Nx()) / ((unsigned int)RAND_MAX + 1u);
+		int y = (random() * grid.Ny()) / ((unsigned int)RAND_MAX + 1u);
+		int z = (random() * grid.Nz()) / ((unsigned int)RAND_MAX + 1u);
+		
+		// Calculate energy change (divided by 2*J)
+		double delta_e = grid(x,y,z) * (
+						    grid(x+1,y  ,z  )
+						  + grid(x  ,y+1,z  )
+						  + grid(x  ,y  ,z+1)
+						  + grid(x-1,y  ,z  )
+						  + grid(x  ,y-1,z  )
+						  + grid(x  ,y  ,z-1)
+			);
+		
+		// flip (or not)
+		if (delta_e <= 0){
+			grid(x,y,z) *= -1;
+		} else {
+			double probability = (exp(-betaTwoJ * delta_e));
+			//std::cerr << probability  << std::endl;
+			if ((double)random()/ (double)(RAND_MAX+1) < probability){
+				grid(x,y,z) *= -1;
 			}
 		}
 	}
