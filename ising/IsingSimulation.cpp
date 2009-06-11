9782ea636680be3d5eb0e2b548672927772e4ba9
Raphael Borun Das Gupta <git@raphael.dasgupta.ch>
2009-06-11 15:46:19 +0200

    auto-generated new class IsingSimulation

---
+++ ising/IsingSimulation.cpp
@@ -0,0 +1,23 @@
+//
+// C++ Implementation: IsingSimulation
+//
+// Description: 
+//
+//
+// Author:  <>, (C) 2009
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "IsingSimulation.h"
+
+IsingSimulation::IsingSimulation()
+{
+}
+
+
+IsingSimulation::~IsingSimulation()
+{
+}
+
+
---
+++ ising/IsingSimulation.h
@@ -0,0 +1,26 @@
+//
+// C++ Interface: IsingSimulation
+//
+// Description: 
+//
+//
+// Author:  <>, (C) 2009
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef ISINGSIMULATION_H
+#define ISINGSIMULATION_H
+
+/**
+	@author 
+*/
+class IsingSimulation{
+public:
+    IsingSimulation();
+
+    ~IsingSimulation();
+
+};
+
+#endif
