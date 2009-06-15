c203c7777e9f674a79062668a1d0e024636d7e17
Raphael Borun Das Gupta <git@raphael.dasgupta.ch>
2009-06-15 16:07:05 +0200

    safety-defines in random.h

--- tools/random.h
+++ tools/random.h
@@ -1,3 +1,23 @@
+//
+// Utility header providing some wrapper functions.
+//
+// Description: Wrapper functions for cstdlib's random().
+//              random() only provides long int pseudo
+//              random numbers between 0 and RAND_MAX
+//              (inclusive). The wrapper functions here
+//              provide doubles between 0.0 and 1.0, as
+//              well as unsigned ints between 0 and a given
+//              parameter (upper bounds exclusive).
+//
+//
+// Author: Raphael Das Gupta <raphaeld@ethz.ch>, (C) 2009
+//
+// Copyright: All rites reversed -- copy what you like.
+//
+//
+#ifndef TOOLS_RANDOM_H
+#define TOOLS_RANDOM_H
+
 #include <cstdlib> // for random()
 
 inline double drand(){
@@ -7,3 +27,5 @@ inline double drand(){
 inline unsigned int uirand(unsigned int max_plus_one){
 	return ((unsigned int)random() * max_plus_one) / ((unsigned int)RAND_MAX + 1u);
 }
+
+#endif // TOOLS_RANDOM_H
