0caa7f02fdfe68ef0279a400b3e2cbb2090ca3e4
Raphael Borun Das Gupta <git@raphael.dasgupta.ch>
2009-06-23 17:44:22 +0200

    data collaps (needs tuning)

--- ising/ex03plots.py
+++ ising/ex03plots.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python
-
+from __future__ import division
 import pylab as pl
 
 pl.plotfile("./max_chi_vs_N.dat", cols=range(3), delimiter='\t')
-pl.savefig("max_chi_vs_N.png")
+pl.savefig("./max_chi_vs_N.png")
+
+## Data collaps
+pl.figure()
+tmp_data = pl.csv2rec("./max_chi_vs_N.dat", delimiter='\t')
+gamma_over_nu_estimation = pl.mean(tmp_data['max_chi'] / tmp_data['system_size'])
+del tmp_data
+
+data = pl.csv2rec("./data_collapse.dat", delimiter='\t')
+kT = data['kt']
+T_C = 4.51
+N = data['system_size']
+chi = data['susceptibility']
+
+gamma_over_nu = gamma_over_nu_estimation
+print gamma_over_nu
+for i in range(40):
+	pl.figure()
+	one_over_nu = (i / 20 - 1.) * 0.7
+	pl.loglog(abs(kT - T_C) * (N ** one_over_nu), chi * (N ** (-gamma_over_nu)), '.')
+	pl.savefig("./data_collapse" +i.__str__()+"__"+ one_over_nu.__str__() + ".png")
--- ising/finite_size_scaling.cpp
+++ ising/finite_size_scaling.cpp
@@ -1,9 +1,10 @@
 #include <fstream> // for output of results
 #include "./IsingSimulation.h"
+#include "../tools/random.h"
 
 //const int T_C = 4.51;
 
-double do_simulation(const double &r_kT, const int &rL){
+double do_simulation(const double &r_kT, const int &rL, std::ostream &output_stream){
 	
 	IsingSimulation sim(rL,r_kT);
 	
@@ -13,14 +14,25 @@ double do_simulation(const double &r_kT, const int &rL){
 	sim.termalize(MC_STEPS/10);
 	sim.run(MC_STEPS);
 	
+	output_stream
+		<< sim.get_system_size() << '\t'
+		<< r_kT << '\t'
+		<< sim.get_mean_energy() << '\t'
+		<< sim.get_mean_magnetization() << '\t'
+		<< sim.get_susceptibility_estimation() << '\t' // susceptibility chi
+		<< sim.get_specific_heat_estimation() << std::endl;  // specific heat C_V
+	
 	return sim.get_susceptibility_estimation();
 }
 
 int main(){
-	// open output file:
+	// open output files:
 	std::ofstream max_chi_file("./max_chi_vs_N.dat");
-	// write title row:
+	std::ofstream data_collapse_file("./data_collapse.dat");
+	
+	// write title rows:
 	max_chi_file << "system_size\t$T_C_of_L$\tmax_chi" << std::endl;
+	data_collapse_file << "system_size\tkT\tenergy\tmagnetization\tsusceptibility\tspecific_heat" << std::endl;
 	
 	// run simulation and write data rows
 	double max_chi;
@@ -31,7 +43,7 @@ int main(){
 		T_C_of_L = -1.;
 		for (double kT = 3.6; kT <= 5.; kT += 0.05){
 			//std::cerr << kT << std::endl;
-			chi = do_simulation(kT, L);
+			chi = do_simulation(kT, L, data_collapse_file);
 			if (chi > max_chi){
 				T_C_of_L = kT;
 				max_chi = chi;
@@ -39,6 +51,13 @@ int main(){
 		}
 		max_chi_file << L * L * L << '\t' << T_C_of_L << '\t' << max_chi << std::endl;
 	}
-	//close output file:
+	
+	// more samples for data collapse
+	for(int i = 1; i <= 200; ++i){
+		do_simulation(0. + drand() * 8., 3 + uirand(17), data_collapse_file);
+	}
+	
+	//close output files:
 	max_chi_file.close();
+	data_collapse_file.close();
 }
