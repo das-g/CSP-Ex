431485b05ec665908260c6cd6ed82c25d7c72041
Raphael Borun Das Gupta <git@raphael.dasgupta.ch>
2009-06-29 13:48:44 +0200

    fixed: discrete data causes gaps in histograms

--- ising/plots.py
+++ ising/plots.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from __future__ import division
+
 import pylab as pl
 
 stat_data = pl.csv2rec("./everything_vs_MaxDeamonEnergy.dat", delimiter='\t')
@@ -10,7 +12,8 @@ data_files = ["./hist_" + max_deamon_energy.__str__() + ".dat" for max_deamon_en
 for data_file in data_files:
 	pl.figure()
 	data = pl.load(data_file)
-	pdf, bins, patches = pl.hist(data, log=True, normed=True)
+	number_of_bins = (data.max() - data.min()) / 2 ## data is only even integers, and we don't want gaps in the histogram
+	pdf, bins, patches = pl.hist(data, log=True, normed=True, bins=number_of_bins)
 	pl.savefig(data_file + ".png")
 
 #pl.plotfile("./everything_vs_MaxDeamonEnergy.dat", cols=range(3), delimiter='\t')
