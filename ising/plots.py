e18867233d0f9ef02bbd0aca543adc959a4ac79e
Raphael Borun Das Gupta <git@raphael.dasgupta.ch>
2009-06-29 13:08:59 +0200

    use exact filenames to find data files instead of pattern matching

--- ising/plots.py
+++ ising/plots.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
-import os
+
 import pylab as pl
 
-## files that contain "hist_"
-data_files = [f for f in os.listdir(os.path.curdir) if "hist_" in f]
+stat_data = pl.csv2rec("./everything_vs_MaxDeamonEnergy.dat", delimiter='\t')
+max_deamon_energies = stat_data["maxdeamonenergy"]
+
+data_files = ["./hist_" + max_deamon_energy.__str__() + ".dat" for max_deamon_energy in max_deamon_energies]
 
 for data_file in data_files:
 	pl.figure()
