0de0def5e259b06beea8a423d14f23ba76a1eaeb
Raphael Borun Das Gupta <git@raphael.dasgupta.ch>
2009-07-01 13:34:55 +0200

    fixed: estimated k_B*T wrong

--- ising/plots.py
+++ ising/plots.py
@@ -35,13 +35,21 @@ for data_file in data_files:
 	number_of_bins = (data.max() - data.min()) / 4
 	pdf, bins, patches = pl.hist(data, log=True, normed=True, bins=number_of_bins)
 	pl.savefig(data_file + ".png")
-	betas[i] = ( - (pdf[-1] - pdf[0]) / (bins[-2] - bins[0]) )
+	
+	## We still have to take the log() of the probability density function,
+	## here, as log=True in hist() only affects the rendering, not the return values.
+	## 
+	## Take the second but last entry of bins, because we want the lower border of the last bin.
+	## 
+	## The multiplication by 2 is because all energies are in 2J units.
+	## (For this calculation, we want it in J)
+	betas[i] = - (pl.log(pdf[-1]) - pl.log(pdf[0])) / ((bins[-2] - bins[0]) * 2)
 	i += 1
 
 pl.figure()
 pl.plot(1 / betas, system_energies, 'r')
 pl.plot(1 / betas, magnetizations, 'b')
-pl.xlabel("$\propto\, k_b\cdot T$")
+pl.xlabel("$k_B\cdot T$")
 pl.ylabel("$E_\mathrm{system}$ (red) and Magnetization (blue), both normalized")
 pl.savefig("everything_vs_kT.png")
 
@@ -49,6 +57,6 @@ pl.savefig("everything_vs_kT.png")
 pl.figure()
 pl.plot(1 / betas[:-2], system_energies[:-2], 'r')
 pl.plot(1 / betas[:-2], magnetizations[:-2], 'b')
-pl.xlabel("$\propto\, k_b\cdot T$")
+pl.xlabel("$k_B\cdot T$")
 pl.ylabel("$E_\mathrm{system}$ (red) and Magnetization (blue), both normalized")
 pl.savefig("everything_vs_kT_2.png")
