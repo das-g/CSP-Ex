a900abdc9d061d4565041489123a7e8beea18034
Raphael Borun Das Gupta <git@raphael.dasgupta.ch>
2009-07-08 14:05:01 +0200

    prettify plots

--- ising/plots.py
+++ ising/plots.py
@@ -15,32 +15,32 @@ for versus in ["kT","system_size"]:
 	
 	## Energies
 	pl.subplot(221)
-	pl.plot(sf_data[versus.lower()], sf_data["energy"], label="single flip")
-	pl.plot(wolff_data[versus.lower()], wolff_data["energy"], label="Wolff")
+	pl.plot(sf_data[versus.lower()], sf_data["energy"], '.-', label="single flip")
+	pl.plot(wolff_data[versus.lower()], wolff_data["energy"], '.-', label="Wolff")
 	pl.ylabel("Energy")
 	pl.xlabel(versus)
 	
 	## Magn.
 	pl.subplot(222)
-	pl.plot(sf_data[versus.lower()], sf_data["magnetization"], label="single flip")
-	pl.plot(wolff_data[versus.lower()], wolff_data["magnetization"], label="Wolff")
+	pl.plot(sf_data[versus.lower()], sf_data["magnetization"], '.-', label="single flip")
+	pl.plot(wolff_data[versus.lower()], wolff_data["magnetization"], '.-', label="Wolff")
 	pl.ylabel("Magn.")
 	pl.xlabel(versus)
 	
 	## Susc. (chi)
 	pl.subplot(223)
-	pl.plot(sf_data[versus.lower()], sf_data["susceptibility"], label="single flip")
-	pl.plot(wolff_data[versus.lower()], wolff_data["susceptibility"], label="Wolff")
+	pl.plot(sf_data[versus.lower()], sf_data["susceptibility"], '.-', label="single flip")
+	pl.plot(wolff_data[versus.lower()], wolff_data["susceptibility"], '.-', label="Wolff")
 	pl.ylabel("Susc. $\chi$")
 	pl.xlabel(versus)
 	
 	## C_V
 	pl.subplot(224)
-	pl.plot(sf_data[versus.lower()], sf_data["specific_heat"], label="single flip")
-	pl.plot(wolff_data[versus.lower()], wolff_data["specific_heat"], label="Wolff")
+	pl.plot(sf_data[versus.lower()], sf_data["specific_heat"], '.-', label="single flip")
+	pl.plot(wolff_data[versus.lower()], wolff_data["specific_heat"], '.-', label="Wolff")
 	pl.ylabel("spec. heat $C_V$")
 	pl.xlabel(versus)
-	pl.legend()
+	pl.legend(loc=(0.6, 0.6))
 	
 	pl.subplots_adjust(wspace=0.35)
 	pl.savefig("everything_vs_" + versus + ".png")
