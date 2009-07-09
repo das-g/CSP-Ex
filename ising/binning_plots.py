139d98f6bbca0f02caf99515f0f8b20a2d1004a1
Raphael Borun Das Gupta <git@raphael.dasgupta.ch>
2009-07-09 15:37:31 +0200

    label binning plot

--- ising/binning_plots.py
+++ ising/binning_plots.py
@@ -5,7 +5,10 @@ import pylab as pl
 data_sf = pl.load("./binning_single_flip.dat", delimiter='\t')
 data_w = pl.load("./binning_wolff.dat", delimiter='\t')
 
-pl.plot(data_sf[:,0], data_sf[:,1])
-pl.plot(data_w[:,0], data_w[:,1])
+pl.plot(data_sf[:,0], data_sf[:,1], '.-', label="single flip")
+pl.plot(data_w[:,0], data_w[:,1], '.-', label="Wolff")
+pl.xlabel("binning level")
+pl.ylabel("energy error")
+pl.legend(loc='best')
 
 pl.savefig("./binning_analysis.png")
